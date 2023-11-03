992ff004e166babd50c99d16df4ae408733c340d
Norsang Nyandak <TNorsang>
2023-11-03 08:30:45 -0400

    added grid

--- best_path.py
+++ best_path.py
@@ -1,7 +1,7 @@
 import pygame
 
-
 # Setting the Screen Height
+width, height = 800, 800
 
 
 def screenSize(width, height):
@@ -9,20 +9,29 @@ def screenSize(width, height):
     return screen
 
 
-screen = screenSize(800, 800)
+screen = screenSize(width, height)
+
+
+def drawGrid(gridSize, gridColor):
+    for x in range(0, width, gridSize):
+        pygame.draw.line(screen, gridColor, (x, 0), (x, height))
+    for y in range(0, height, gridSize):
+        pygame.draw.line(screen, gridColor, (0, y), (width, y))
 
 
 def runGame():
     pygame.init()
 
     backgroundColor = (255, 255, 255)
+    pygame.display.set_caption("Best Path")
     running = True
 
     while running:
         for event in pygame.event.get():
-            if event.type == ßßpygame.QUIT:
+            if event.type == pygame.QUIT:
                 running = False
         screen.fill(backgroundColor)
+        drawGrid(50, (200, 200, 200))
         pygame.display.flip()
 
 
